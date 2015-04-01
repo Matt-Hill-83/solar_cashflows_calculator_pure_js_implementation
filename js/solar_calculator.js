5d2857a16710cce21d146990d7b9b9911329176b
KletusVandamme <matt.hill.email@gmail.com>
2015-04-01 05:08:52 -0700

    refactoring

--- js/solar_calculator.js
+++ js/solar_calculator.js
@@ -6,6 +6,7 @@ String.prototype.insertComma = function() {
 };
 
 function drawChart() {
+
   var data = google.visualization.arrayToDataTable([
     ['Expense', 'Per Month'],
     ['Principal & Interest',     2912],
@@ -20,59 +21,62 @@ function drawChart() {
     pieSliceTextStyle: {
     color: 'transparent',
     },
-
   };
-
-function getValuesFromSliders(){
-  this.homePrice = document.getElementById("home-price-slider").value;
-  this.downPayment = document.getElementById("down-payment-slider").value;
-  this.interestRate = Number(document.getElementById("interest-rate-slider").value);
-}
-
-function updateNumericResults() {
-    // Set result field next to slider value
-    var homePriceResultEl = document.getElementById("home-price-result");
-    homePriceResultEl.innerHTML = this.homePrice.insertComma();
-
-    var downPaymentResultEl = document.getElementById("down-payment-result");
-    downPaymentResultEl.innerHTML = this.downPayment;
-
-    var interestRateResultEl = document.getElementById("interest-rate-result");
-    interestRateResultEl.innerHTML = this.interestRate;
-};
-
-function calculateMonthlyPayment(){
-  // Breakout of monthly payment calculation
-  var monthlyInterestRate = this.interestRate/12/100;
-  var termInYears = 1;
-  var loanAmount = this.homePrice - (this.downPayment/100)*this.homePrice;
-  // var monthlyPayment;
-
-  var numerator = monthlyInterestRate *(Math.pow(1.0 + monthlyInterestRate, 12.0 * termInYears));
-  var denominator = Math.pow((1 + monthlyInterestRate), (12.0 * termInYears)) - 1.0;
-  var numOverDenom = numerator/denominator;
-  var monthlyPayment = loanAmount * numOverDenom;
-  this.monthlyPaymentStr = monthlyPayment.toFixed(0).insertComma();
-
-}
-
-function updateDisplay() {
-  getValuesFromSliders();
-  updateNumericResults();
-  calculateMonthlyPayment();
-
-  var monthlyPaymentResult = document.getElementById("monthly-payment-result");
-  monthlyPaymentResult.innerHTML = '$' + this.monthlyPaymentStr;
-
-  this.chart.draw(data, options);
-  };
-
- this.chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  // chart.draw(data, options);
-  updateDisplay();
 
   // Set up UI events.
   document.getElementById("home-price-slider").onchange = function (){updateDisplay()};
   document.getElementById("down-payment-slider").onchange = function (){updateDisplay()};
   document.getElementById("interest-rate-slider").onchange = function (){updateDisplay()};
-}
+
+  this.chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+
+  function getValuesFromSliders(){
+    this.homePrice = document.getElementById("home-price-slider").value;
+    this.downPayment = document.getElementById("down-payment-slider").value;
+    this.interestRate = Number(document.getElementById("interest-rate-slider").value);
+  };
+
+  function updateNumericResults() {
+      // Set result field next to slider value
+      var homePriceResultEl = document.getElementById("home-price-result");
+      homePriceResultEl.innerHTML = this.homePrice.insertComma();
+
+      var downPaymentResultEl = document.getElementById("down-payment-result");
+      downPaymentResultEl.innerHTML = this.downPayment;
+
+      var interestRateResultEl = document.getElementById("interest-rate-result");
+      interestRateResultEl.innerHTML = this.interestRate;
+  };
+
+  function calculateMonthlyPayment(){
+    // Breakout of monthly payment calculation
+    var monthlyInterestRate = this.interestRate/12/100;
+    var termInYears = 1;
+    var loanAmount = this.homePrice - (this.downPayment/100)*this.homePrice;
+
+    var numerator = monthlyInterestRate *(Math.pow(1.0 + monthlyInterestRate, 12.0 * termInYears));
+    var denominator = Math.pow((1 + monthlyInterestRate), (12.0 * termInYears)) - 1.0;
+    var numOverDenom = numerator/denominator;
+    var monthlyPayment = loanAmount * numOverDenom;
+    this.monthlyPaymentStr = monthlyPayment.toFixed(0).insertComma();
+
+  };
+
+  function updateMonthlyPaymentResults(){
+    var monthlyPaymentResult = document.getElementById("monthly-payment-result");
+    monthlyPaymentResult.innerHTML = '$' + this.monthlyPaymentStr;
+
+  }
+
+  function updateDisplay() {
+    getValuesFromSliders();
+    updateNumericResults();
+    calculateMonthlyPayment();
+    updateMonthlyPaymentResults();
+
+    this.chart.draw(data, options);
+  };
+
+    updateDisplay();
+
+};
